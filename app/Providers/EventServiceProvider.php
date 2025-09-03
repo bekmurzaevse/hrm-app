01d0df142e2124543f347d1c9825aa8cbe19552e
simpledevweb <ibraymovalpamis81@gmail.com>
2025-09-03 18:43:46 +0500

    ref: add Observer for Region and District; deleted FilterServiceProvider

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -26,5 +26,8 @@ class EventServiceProvider extends ServiceProvider
         \App\Models\User::observe(\App\Observers\UserObserver::class);
         \App\Models\Project::observe(\App\Observers\ProjectObserver::class);
         \App\Models\Vacancy::observe(\App\Observers\VacancyObserver::class);
+
+        \App\Models\Region::observe(\App\Observers\RegionObserver::class);
+        \App\Models\District::observe(\App\Observers\DistrictObserver::class);
     }
 }
