a60d5d505cc7cdcf0cbb8cf810dbf6982bbdce07
simpledevweb <ibraymovalpamis81@gmail.com>
2025-09-15 17:09:25 +0500

    ref: refactor Task model, CreateRequest and IndexAction

--- app/Http/Requests/v1/Task/CreateRequest.php
+++ app/Http/Requests/v1/Task/CreateRequest.php
@@ -30,7 +30,7 @@ class CreateRequest extends FormRequest
         return [
             'title' => 'required|string|min:2|max:255',
             'description' => 'nullable|string',
-            'deadline' => 'required|date_format:m-d-Y',
+            'deadline' => 'required|date_format:Y-m-d',
             'status' => ['required', Rule::enum(TaskStatusEnum::class)],
             'priority' => ['required', Rule::enum(TaskPriorityEnum::class)],
             'comment' => 'nullable|string|max:1000',
