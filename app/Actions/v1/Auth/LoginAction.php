e242753fabda9d21c733de406106d6cc97c9a55b
simpledevweb <ibraymovalpamis81@gmail.com>
2025-09-17 15:45:37 +0500

    ref: optimized LoginAction

--- app/Actions/v1/Auth/LoginAction.php
+++ app/Actions/v1/Auth/LoginAction.php
@@ -8,7 +8,6 @@ use App\Exceptions\ApiResponseException;
 use App\Http\Resources\v1\Auth\GetMeResource;
 use App\Models\User;
 use App\Traits\ResponseTrait;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Hash;
 
@@ -24,42 +23,38 @@ class LoginAction
      */
     public function __invoke(LoginDto $dto): JsonResponse
     {
-        try {
-            $user = User::where('email', $dto->email)->firstOrFail();
+        $user = User::where('email', $dto->email)->first();
 
-            if (!Hash::check($dto->password, $user->password)) {
-                throw new ApiResponseException('Email or password is incorrect', 401);
-            }
-
-            auth()->login($user);
-
-            $accessTokenExpiration = now()->addMinutes(config('sanctum.at_expiration'));
-            $refreshTokenExpiration = now()->addMinutes(config('sanctum.rt_expiration'));
-
-            $accessToken = auth()->user()->createToken(
-                name: 'access token',
-                abilities: [TokenAbilityEnum::ACCESS_TOKEN->value],
-                expiresAt: $accessTokenExpiration
-            );
-
-            $refreshToken = auth()->user()->createToken(
-                name: 'refresh token',
-                abilities: [TokenAbilityEnum::ISSUE_ACCESS_TOKEN->value],
-                expiresAt: $refreshTokenExpiration
-            );
-
-            return static::toResponse(
-                message: "User logged in successfully",
-                data: [
-                    'access_token' => $accessToken->plainTextToken,
-                    'refresh_token' => $refreshToken->plainTextToken,
-                    'at_expired_at' => $accessTokenExpiration->format('Y-m-d H:i:s'),
-                    'rf_expired_at' => $refreshTokenExpiration->format('Y-m-d H:i:s'),
-                    'user' => new GetMeResource($user)
-                ]
-            );
-        } catch (ModelNotFoundException $ex) {
+        if (!$user || !Hash::check($dto->password, $user->password)) {
             throw new ApiResponseException('Email or password is incorrect', 401);
         }
+
+        auth()->login($user);
+
+        $accessTokenExpiration = now()->addMinutes(config('sanctum.at_expiration'));
+        $refreshTokenExpiration = now()->addMinutes(config('sanctum.rt_expiration'));
+
+        $accessToken = auth()->user()->createToken(
+            name: 'access token',
+            abilities: [TokenAbilityEnum::ACCESS_TOKEN->value],
+            expiresAt: $accessTokenExpiration
+        );
+
+        $refreshToken = auth()->user()->createToken(
+            name: 'refresh token',
+            abilities: [TokenAbilityEnum::ISSUE_ACCESS_TOKEN->value],
+            expiresAt: $refreshTokenExpiration
+        );
+
+        return static::toResponse(
+            message: "User logged in successfully",
+            data: [
+                'access_token' => $accessToken->plainTextToken,
+                'refresh_token' => $refreshToken->plainTextToken,
+                'at_expired_at' => $accessTokenExpiration->format('Y-m-d H:i:s'),
+                'rf_expired_at' => $refreshTokenExpiration->format('Y-m-d H:i:s'),
+                'user' => new GetMeResource($user)
+            ]
+        );
     }
 }
